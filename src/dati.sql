5451cdc9ab1dea37b7355c2738250c42f023cbb5
StefanoElisio <stfnelisio.work@gmail.com>
2024-09-03 12:04:27 +0200

    Create dati.sql + add constraints names

---
+++ src/dati.sql
@@ -0,0 +1,133 @@
+USE market;
+
+SET FOREIGN_KEY_CHECKS = 0;
+
+TRUNCATE TABLE caratteristica;
+TRUNCATE TABLE categoria;
+TRUNCATE TABLE ordinante;
+TRUNCATE TABLE prodotto_candidato;
+TRUNCATE TABLE richiesta_acquisto;
+TRUNCATE TABLE richiesta_info_caratteristiche;
+TRUNCATE TABLE richiesta_definita_categoria;
+TRUNCATE TABLE richiesta_relativo_prodotto;
+TRUNCATE TABLE tecnico;
+TRUNCATE TABLE tecnico_scelta_prodotto;
+TRUNCATE TABLE valutazione;
+
+SET FOREIGN_KEY_CHECKS = 1;
+
+INSERT INTO ordinante(username, nome, cognome)
+VALUES 
+    ('Fragola86', NULL, NULL),
+    ('SuperMario', 'Mario', 'Rossi'),
+    ('Fili_Gio', 'Filippo', 'Di giovanni');
+
+INSERT INTO tecnico(username, nome, cognome)
+VALUES 
+    ('Agente007', 'James', 'Bond'),
+    ('Alegorico', 'Alessando', 'Angelini'),
+    ('Tecnico esperto', NULL, NULL);
+
+INSERT INTO prodotto_candidato(
+        numero_seriale,
+        nome,
+        nome_produttore,
+        prezzo,
+        url_approfondimento,
+        note
+    )
+VALUES (
+        '16cva8b18sca987h1',
+        'MecBook Pro Max',
+        'Apple',
+        9999.98,
+        'https://www.apple.com/it/macbook-pro',
+        'Costa tanto e non ne vale la pena, ma se non lo fai cadere ti durerà, credo'
+    ),
+    (
+        'caijbsib1ijscb1jciasbc',
+        'AirForce1',
+        'Nike',
+        107.89,
+        'https://www.nike.com/it/w/air-force-1',
+        NULL
+    ),
+    (
+        'ncq91ckihhc918s1xb19',
+        'Giacca in pelle',
+        'Calvin Klain',
+        599.90,
+        'https://www.calvinklein.it/giubbotti-stile-motociclista-in-pelle',
+        NULL
+    );
+
+INSERT INTO categoria(nome)
+VALUES
+    ('Elettronica'),
+    ('SmartPhone'), 
+    ('PC portatili'), 
+    ('Abbigliamento'),
+    ('Scarpe'),
+    ('Giubbetti');
+
+INSERT INTO richiesta_acquisto (ID_ordinante, ID_tecnico, tempo, note, esito)
+VALUES 
+    (1, 1, '2024-06-01 12:34:56', 'Richiesta urgente', 'accettato');
+INSERT INTO richiesta_acquisto (ID, ID_ordinante, ID_tecnico, note, esito)
+VALUES 
+    (2, 1, 3, 'Costo non maggiore di 1000$', 'respinto perché non conforme'),
+    (3, 3, 2, NULL, 'respinto perché non funzionante');
+
+INSERT INTO caratteristica(ID_categoria, nome)
+VALUES
+    (1,'Autonomia'),
+    (2,'Pollici'),
+    (2,'MegaPixel'),
+    (3,'CPU'),
+    (3,'GPU'),
+    (4,'Genere'),
+    (5,'Taglia'),
+    (6,'Taglia'),
+    (6,'Colore');
+
+INSERT INTO valutazione(ID_prodotto_candidato,ID_ordinante,decisione,motivazione)
+VALUES
+    (3, 2, 'rifiutato', 'Il prodotto ha superato il budget'),
+    (2, 1, 'approvato', NULL),
+    (1, 3, 'approvato', NULL),
+    (1, 1, 'rifiutato', 'Il prodotto non soddisfa i requisiti tecnici');
+
+INSERT INTO tecnico_scelta_prodotto (ID_tecnico, ID_prodotto)
+VALUES
+    (1,2),
+    (2,1),
+    (2,3),
+    (1,2),
+    (3,3);
+
+INSERT INTO richiesta_relativo_prodotto(ID_richiesta_acquisto,ID_prodotto)
+VALUES
+    (1,1),
+    (1,2),
+    (2,3),
+    (3,2);
+
+INSERT INTO richiesta_definita_categoria(ID_richiesta_acquisto,ID_categoria)
+VALUES
+    (1,1),
+    (1,3),
+    (2,4),
+    (3,4),
+    (3,5);
+
+INSERT INTO richiesta_info_caratteristiche(ID_richiesta_acquisto,ID_caratteristica)
+VALUES
+    (1,1),
+    (1,3),
+    (2,6);
+INSERT INTO richiesta_info_caratteristiche(ID_richiesta_acquisto,ID_caratteristica,valore)
+VALUES
+    (1,2,'almeno 12'),
+    (3,6,'uomo'),
+    (3,7,'media');
+
--- src/structure.sql
+++ src/structure.sql
@@ -30,24 +30,24 @@ CREATE TABLE richiesta_acquisto (
     ID INT AUTO_INCREMENT PRIMARY KEY,
     ID_ordinante INT NOT NULL,
     ID_tecnico INT NOT NULL,
-    tempo TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    tempo TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     note TEXT,
     esito ENUM(
         'accettato',
         'respinto perché non conforme',
         'respinto perché non funzionante'
     ) NOT NULL,
-    FOREIGN KEY (ID_ordinante) REFERENCES ordinante(ID) ON UPDATE CASCADE ON DELETE RESTRICT,
-    FOREIGN KEY (ID_tecnico) REFERENCES tecnico(ID) ON UPDATE CASCADE ON DELETE RESTRICT,
-    UNIQUE(ID_ordinante, tempo),
-    UNIQUE(ID_tecnico, tempo)
+    CONSTRAINT richiesta_ordinante FOREIGN KEY (ID_ordinante) REFERENCES ordinante(ID) ON UPDATE CASCADE ON DELETE RESTRICT,
+    CONSTRAINT richiesta_tecnico FOREIGN KEY (ID_tecnico) REFERENCES tecnico(ID) ON UPDATE CASCADE ON DELETE RESTRICT,
+    CONSTRAINT un_ordinante UNIQUE(ID_ordinante, tempo),
+    CONSTRAINT un_tecnico UNIQUE(ID_tecnico, tempo)
 );
 CREATE TABLE caratteristica (
     ID INT AUTO_INCREMENT PRIMARY KEY,
     ID_categoria INT NOT NULL,
     nome VARCHAR(20) NOT NULL,
-    FOREIGN KEY (ID_categoria) REFERENCES categoria(ID) ON UPDATE CASCADE ON DELETE CASCADE,
-    UNIQUE(ID_categoria, nome)
+    CONSTRAINT caratteristica_categoria FOREIGN KEY (ID_categoria) REFERENCES categoria(ID) ON UPDATE CASCADE ON DELETE RESTRICT,
+    CONSTRAINT caratteristica_unica UNIQUE(ID_categoria, nome)
 );
 CREATE TABLE valutazione (
     ID INT AUTO_INCREMENT PRIMARY KEY,
@@ -55,46 +55,46 @@ CREATE TABLE valutazione (
     ID_ordinante INT NOT NULL,
     decisione ENUM('approvato', 'rifiutato') NOT NULL,
     motivazione TEXT,
-    FOREIGN KEY (ID_prodotto_candidato) REFERENCES prodotto_candidato(ID) ON UPDATE CASCADE ON DELETE RESTRICT,
-    FOREIGN KEY (ID_ordinante) REFERENCES ordinante(ID) ON UPDATE CASCADE ON DELETE RESTRICT,
-    CHECK (
+    CONSTRAINT valutazione_prodotto FOREIGN KEY (ID_prodotto_candidato) REFERENCES prodotto_candidato(ID) ON UPDATE CASCADE ON DELETE RESTRICT,
+    CONSTRAINT valutazione_ordinante FOREIGN KEY (ID_ordinante) REFERENCES ordinante(ID) ON UPDATE CASCADE ON DELETE RESTRICT,
+    CONSTRAINT approvato CHECK (
         decisione = 'approvato'
         AND motivazione IS NULL
     ),
-    CHECK(
+    CONSTRAINT rifiutato CHECK(
         decisione = 'rifiutato'
         AND motivazione IS NOT NULL
     )
 );
-CREATE TABLE tecnico_prodotti (
+CREATE TABLE tecnico_scelta_prodotto (
     ID INT AUTO_INCREMENT PRIMARY KEY,
     ID_tecnico INT NOT NULL,
     ID_prodotto INT NOT NULL,
-    FOREIGN KEY (ID_tecnico) REFERENCES tecnico(ID) ON UPDATE CASCADE ON DELETE RESTRICT,
-    FOREIGN KEY (ID_prodotto) REFERENCES prodotto_candidato(ID) ON UPDATE CASCADE ON DELETE RESTRICT
+    CONSTRAINT tecnico_scelto FOREIGN KEY (ID_tecnico) REFERENCES tecnico(ID) ON UPDATE CASCADE ON DELETE RESTRICT,
+    CONSTRAINT prodotto_scelto FOREIGN KEY (ID_prodotto) REFERENCES prodotto_candidato(ID) ON UPDATE CASCADE ON DELETE RESTRICT
 );
-CREATE TABLE richiesta_prodotti (
+CREATE TABLE richiesta_relativo_prodotto (
     ID INT AUTO_INCREMENT PRIMARY KEY,
     ID_richiesta_acquisto INT NOT NULL,
     ID_prodotto int NOT NULL,
-    UNIQUE (ID_richiesta_acquisto, ID_prodotto),
-    FOREIGN KEY (ID_richiesta_acquisto) REFERENCES richiesta_acquisto(ID) ON UPDATE CASCADE ON DELETE CASCADE,
-    FOREIGN KEY (ID_prodotto) REFERENCES prodotto_candidato(ID) ON UPDATE CASCADE ON DELETE RESTRICT
+    CONSTRAINT richiesta_prodotto_unica UNIQUE (ID_richiesta_acquisto, ID_prodotto),
+    CONSTRAINT richiesta_relativa FOREIGN KEY (ID_richiesta_acquisto) REFERENCES richiesta_acquisto(ID) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT prodotto_relativo FOREIGN KEY (ID_prodotto) REFERENCES prodotto_candidato(ID) ON UPDATE CASCADE ON DELETE RESTRICT
 );
-CREATE TABLE richiesta_categoria (
+CREATE TABLE richiesta_definita_categoria (
     ID INT AUTO_INCREMENT PRIMARY KEY,
     ID_richiesta_acquisto INT NOT NULL,
     ID_categoria int NOT NULL,
-    UNIQUE (ID_richiesta_acquisto, ID_categoria),
-    FOREIGN KEY (ID_richiesta_acquisto) REFERENCES richiesta_acquisto(ID) ON UPDATE CASCADE ON DELETE CASCADE,
-    FOREIGN KEY (ID_categoria) REFERENCES categoria(ID) ON UPDATE CASCADE ON DELETE RESTRICT
+    CONSTRAINT richiesta_categoria_unica UNIQUE (ID_richiesta_acquisto, ID_categoria),
+    CONSTRAINT richiesta_definita FOREIGN KEY (ID_richiesta_acquisto) REFERENCES richiesta_acquisto(ID) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT categoria_definita FOREIGN KEY (ID_categoria) REFERENCES categoria(ID) ON UPDATE CASCADE ON DELETE RESTRICT
 );
-CREATE TABLE richiesta_caratteristiche (
+CREATE TABLE richiesta_info_caratteristiche (
     ID INT AUTO_INCREMENT PRIMARY KEY,
     ID_richiesta_acquisto INT NOT NULL,
     ID_caratteristica INT NOT NULL,
     valore VARCHAR(100) DEFAULT 'indifferente',
-    UNIQUE (ID_richiesta_acquisto, ID_caratteristica),
-    FOREIGN KEY (ID_richiesta_acquisto) REFERENCES richiesta_acquisto(ID) ON UPDATE CASCADE ON DELETE CASCADE,
-    FOREIGN KEY (ID_caratteristica) REFERENCES caratteristica(ID) ON UPDATE CASCADE ON DELETE
+    CONSTRAINT richiesta_caratteristiche_unica UNIQUE (ID_richiesta_acquisto, ID_caratteristica),
+    CONSTRAINT richiesta_info FOREIGN KEY (ID_richiesta_acquisto) REFERENCES richiesta_acquisto(ID) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT caratteristica_info FOREIGN KEY (ID_caratteristica) REFERENCES caratteristica(ID) ON UPDATE CASCADE ON DELETE RESTRICT
 );
